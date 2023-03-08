4d9f857159e53badd464db8887210bb749e7338f
Christian Baird <christian.a.baird@gmail.com>
2023-03-07 22:56:36 -0500

    Adding an information component with a header and children

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -2,6 +2,7 @@ import ASU from "../assets/asu.jpg";
 import "../styles/Education.css";
 import { SkillList } from "../helpers/SkillList";
 import SkillItem from "../helpers/SkillItem";
+import Information from "../components/Information";
 
 function Education() {
   return (
@@ -11,21 +12,22 @@ function Education() {
         style={{ backgroundImage: `url(${ASU})` }}>
       </div>
       <div className="rightSide">
-        <h1>EDUCATION</h1>
-        <h2>Bachelor of Science in Information Technology</h2>
-        <h2>Summa Cum Laude honors in the Ira A. Fulton Schools of Engineering</h2>
-        <h2>Skills acquired while pursuing degree:</h2>
+        <Information header="Education">
+          <h2>Bachelor of Science in Information Technology</h2>
+          <h2>Summa Cum Laude honors in the Ira A. Fulton Schools of Engineering</h2>
+          <h2>Skills acquired while pursuing degree:</h2>
 
-        <div className="skillList"> 
-          {SkillList.map((skillItem, key) => {
-            return (
-              <SkillItem
-                key={key}
-                name={skillItem.name}
-              />
-            );
-          })}
-        </div>
+          <div className="skillList"> 
+            {SkillList.map((skillItem, key) => {
+              return (
+                <SkillItem
+                  key={key}
+                  name={skillItem.name}
+                />
+              );
+            })}
+          </div>
+        </Information>
       </div>
     </div>
   );
